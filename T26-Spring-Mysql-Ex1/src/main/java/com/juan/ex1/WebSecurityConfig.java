227925e5ae9a94bf4fdc256365fc6e898d97f1ff
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-23 22:34:09 +0200

    Update WebSecurityConfig.java

--- T26-Spring-Mysql-Ex1/src/main/java/com/juan/ex1/WebSecurityConfig.java
+++ T26-Spring-Mysql-Ex1/src/main/java/com/juan/ex1/WebSecurityConfig.java
@@ -1,18 +1,22 @@
 package com.juan.ex1;
+
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig{
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-		.anyRequest().authenticated()
-		.and().formLogin().permitAll()
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated()
 		.and().httpBasic();
+		
+		return http.build();
 	}
 }
