e95dcb40b88024b8eec27c3e4f242e478975c130
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-23 16:53:05 +0200

    Estructura Proyecto Maven

---
+++ T26-Spring-Mysql-Ex1/src/main/java/com/juan/ex1/T26SpringMysqlEx1Application.java
@@ -0,0 +1,13 @@
+package com.juan.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T26SpringMysqlEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T26SpringMysqlEx1Application.class, args);
+	}
+
+}
---
+++ T26-Spring-Mysql-Ex1/src/test/java/com/juan/ex1/T26SpringMysqlEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T26SpringMysqlEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
